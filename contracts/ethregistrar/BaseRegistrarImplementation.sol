002574ebd7e26b06b26101f974fa3ddca9d0f97d
madeinusmate <cantu.ste@gmail.com>
2022-09-12 18:11:03 +0800

    typo fix

--- contracts/ethregistrar/BaseRegistrarImplementation.sol
+++ contracts/ethregistrar/BaseRegistrarImplementation.sol
@@ -4,32 +4,36 @@ import "../registry/ENS.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "./BaseRegistrar.sol";
-contract BaseRegistrarImplementation is ERC721, BaseRegistrar  {
-    bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(bytes4)"));
-    bytes4 constant private ERC721_ID = bytes4(
-        keccak256("balanceOf(address)") ^
-        keccak256("ownerOf(uint256)") ^
-        keccak256("approve(address,uint256)") ^
-        keccak256("getApproved(uint256)") ^
-        keccak256("setApprovalForAll(address,bool)") ^
-        keccak256("isApprovedForAll(address,address)") ^
-        keccak256("transferFrom(address,address,uint256)") ^
-        keccak256("safeTransferFrom(address,address,uint256)") ^
-        keccak256("safeTransferFrom(address,address,uint256,bytes)")
-    );
-    bytes4 constant private RECLAIM_ID = bytes4(keccak256("reclaim(uint256,address)"));
 
-    constructor(ENS _ens, bytes32 _baseNode) ERC721("","") {
+contract BaseRegistrarImplementation is ERC721, BaseRegistrar {
+    bytes4 private constant INTERFACE_META_ID =
+        bytes4(keccak256("supportsInterface(bytes4)"));
+    bytes4 private constant ERC721_ID =
+        bytes4(
+            keccak256("balanceOf(address)") ^
+                keccak256("ownerOf(uint256)") ^
+                keccak256("approve(address,uint256)") ^
+                keccak256("getApproved(uint256)") ^
+                keccak256("setApprovalForAll(address,bool)") ^
+                keccak256("isApprovedForAll(address,address)") ^
+                keccak256("transferFrom(address,address,uint256)") ^
+                keccak256("safeTransferFrom(address,address,uint256)") ^
+                keccak256("safeTransferFrom(address,address,uint256,bytes)")
+        );
+    bytes4 private constant RECLAIM_ID =
+        bytes4(keccak256("reclaim(uint256,address)"));
+
+    constructor(ENS _ens, bytes32 _baseNode) ERC721("", "") {
         ens = _ens;
         baseNode = _baseNode;
     }
 
-    modifier live {
+    modifier live() {
         require(ens.owner(baseNode) == address(this));
         _;
     }
 
-    modifier onlyController {
+    modifier onlyController() {
         require(controllers[msg.sender]);
         _;
     }
@@ -51,8 +55,8 @@ contract BaseRegistrarImplementation is ERC721, BaseRegistrar  {
         ens.setResolver(baseNode, resolver);
     }
 
-    // Returns true iff the specified name is available for registration.
-    function available(uint256 id) public view override returns(bool) {
+    // Returns true if the specified name is available for registration.
+    function available(uint256 id) public view override returns (bool) {
         // Not available if it's registered here
         return !_exists(id);
     }
@@ -63,7 +67,7 @@ contract BaseRegistrarImplementation is ERC721, BaseRegistrar  {
      * @param owner The address that should own the registration.
      */
     function register(uint256 id, address owner) external override {
-       _register(id, owner, true);
+        _register(id, owner, true);
     }
 
     /**
@@ -72,14 +76,18 @@ contract BaseRegistrarImplementation is ERC721, BaseRegistrar  {
      * @param owner The address that should own the registration.
      */
     function registerOnly(uint256 id, address owner) external {
-       _register(id, owner, false);
+        _register(id, owner, false);
     }
 
-    function _register(uint256 id, address owner, bool updateRegistry) internal live onlyController {
+    function _register(
+        uint256 id,
+        address owner,
+        bool updateRegistry
+    ) internal live onlyController {
         require(available(id));
 
         _mint(owner, id);
-        if(updateRegistry) {
+        if (updateRegistry) {
             ens.setSubnodeOwner(baseNode, bytes32(id), owner);
         }
 
@@ -94,9 +102,15 @@ contract BaseRegistrarImplementation is ERC721, BaseRegistrar  {
         ens.setSubnodeOwner(baseNode, bytes32(id), owner);
     }
 
-    function supportsInterface(bytes4 interfaceID) public override(ERC721, IERC165) view returns (bool) {
-        return interfaceID == INTERFACE_META_ID ||
-               interfaceID == ERC721_ID ||
-               interfaceID == RECLAIM_ID;
+    function supportsInterface(bytes4 interfaceID)
+        public
+        view
+        override(ERC721, IERC165)
+        returns (bool)
+    {
+        return
+            interfaceID == INTERFACE_META_ID ||
+            interfaceID == ERC721_ID ||
+            interfaceID == RECLAIM_ID;
     }
 }
